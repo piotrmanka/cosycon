4e3428e55bf9e6d1580dc00592ac0b18f95da8b8
mankapiotr <mankapiotr@gmail.com>
2019-12-26 20:11:34 +0100

    Delete index.php

--- com/index.php
+++
@@ -1,4 +0,0 @@
-<style>
-@import url('https://fonts.googleapis.com/css?family=Roboto:100&display=swap');
-</style>
-<h1 style="text-align: center; padding: 20% 0; font-family: 'Roboto', sans-serif; font-size:20px">COSYCON</br></br>coming soon !</h1>
