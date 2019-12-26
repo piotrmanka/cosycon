9e7bd184f964f91b0de8072e35f908e3f6456ec8
mankapiotr <mankapiotr@gmail.com>
2019-12-26 20:09:58 +0100

    Już wkrótce

---
+++ pl/index.php
@@ -0,0 +1,4 @@
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto:100&display=swap');
+</style>
+<h1 style="text-align: center; padding: 20% 0; font-family: 'Roboto', sans-serif; font-size:20px">COSYCON</br></br>już wkrótce !</h1>
